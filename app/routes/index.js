ec395a1fdeab0291c66bc707bd5421a31ec42732
Frank Bassard <f.bassard@gmail.com>
2015-05-05 16:56:12 +1200

    Route refactor and data optimisation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-	  var fetched =  Ember.$.getJSON('data/articles.json').then(function(articles) {
+	  return Ember.$.getJSON('data/articles.json').then(function(articles) {
 		  var tags = [];
 		  for (var i = 0; i < articles.length; i++) {
 			  tags = tags.concat(articles[i].tags);
@@ -10,6 +10,5 @@ App.IndexRoute = Ember.Route.extend({
 			  tags : tags
 		  };
 	  });
-	  return fetched;
   }
 });
