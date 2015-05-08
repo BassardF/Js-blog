b15ffcec31fd110c9ca3718f3f27fa74066b2b24
Frank Bassard <f.bassard@gmail.com>
2015-05-08 19:37:56 +1200

    All types and code Highlighting

--- app/app.js
+++ app/app.js
@@ -8,6 +8,27 @@ Ember.Handlebars.helper('content', function(item, options) {
 		return new Handlebars.SafeString('<h2>' + item.value + '<h2>');
 	} else if(item.type === 'p'){
 		return new Handlebars.SafeString('<p>' + item.value + '<p>');
+	} else if(item.type === 'img'){
+		return new Handlebars.SafeString('<img src="assets/img/' + item.value + '">');
+	} else if(item.type === 'quote'){
+		return new Handlebars.SafeString('<div class="quote"><div class="inner">' + item.value + '</div><div class="author">' + item.author + '</div></div>');
+	} else if(item.type === 'code'){
+		return new Handlebars.SafeString('<pre class="brush: ' + item.syntax + '">' + item.value +'</pre>');
+	} else if(item.type === 'list'){
+		var ul = '<ul>#</ul>',
+			li = '<li>#</li>',
+			lis = '';
+			
+		for (var i = 0; i < item.value.length; i++) {
+			lis += li.replace('#', item.value[i]);
+		}
+		
+		lis = ul.replace('#', lis);
+		return new Handlebars.SafeString('<div>' + item.line + lis + '<div>');
 	}
+	
+			
+				
+			
  
 });
